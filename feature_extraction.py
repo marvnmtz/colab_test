50213ca06a324aa0df43f33d3a0eb8cfae3db123
Marvin Mutz <marvin.mutz@tuhh.de>
2021-11-30 11:37:36 +0100

    Adapting to Colab

--- feature_extraction.py
+++ feature_extraction.py
@@ -5,6 +5,7 @@ import random
 import pandas as pd
 from skimage.feature import greycomatrix, greycoprops
 from multiprocessing import Pool
+import timeit
 
 from lib.preprocessing.compute_features import *
 
@@ -41,19 +42,28 @@ if __name__ == '__main__':
     imglist = os.listdir(path_img)
     imglist = random.sample(imglist,10)  # Uncomment for testing
     
-    # create sublists
-    length_imglist = len(imglist)
-    imglist1 = imglist[0:int(length_imglist/4)]
-    imglist2 = imglist[int(length_imglist/4):int(length_imglist/2)]
-    imglist3 = imglist[int(length_imglist/2):3*int(length_imglist/4)]
-    imglist4 = imglist[3*int(length_imglist/4):length_imglist]
+    start = timeit.default_timer()
+    if colab == True:
+        #%% Without Multiprocessing
+        df_features = compute_features(path_img, imglist, ground_truth, traindata)
     
-    # process data in parallel
-    #df_features = compute_features(path_img, imglist, ground_truth, traindata)
-    r = []
-    with Pool(processes=4) as pool:
-        r = pool.starmap(compute_features, [(path_img, imglist1, ground_truth, traindata),(path_img, imglist2, ground_truth, traindata),(path_img, imglist3, ground_truth, traindata),(path_img, imglist4, ground_truth, traindata)])
-    #r = compute_features(path_img, imglist1, ground_truth)
-    df_features = pd.concat(r, ignore_index=True)
+    else:
+        #%% With Multiprocessing
+        # create sublists
+        length_imglist = len(imglist)
+        imglist1 = imglist[0:int(length_imglist/4)]
+        imglist2 = imglist[int(length_imglist/4):int(length_imglist/2)]
+        imglist3 = imglist[int(length_imglist/2):3*int(length_imglist/4)]
+        imglist4 = imglist[3*int(length_imglist/4):length_imglist]
         
+        # process data in parallel
+        #df_features = compute_features(path_img, imglist, ground_truth, traindata)
+        r = []
+        with Pool(processes=4) as pool:
+            r = pool.starmap(compute_features, [(path_img, imglist1, ground_truth, traindata),(path_img, imglist2, ground_truth, traindata),(path_img, imglist3, ground_truth, traindata),(path_img, imglist4, ground_truth, traindata)])
+        #r = compute_features(path_img, imglist1, ground_truth)
+        df_features = pd.concat(r, ignore_index=True)
+    
+    stop = timeit.default_timer()
+    print('Time for feature extraction: ', stop - start)
     df_features.to_pickle("df_features.pkl")
